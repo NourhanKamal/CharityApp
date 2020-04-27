56fee8c1353974b63713740fa4e32417570df209
Mahmoud-Abdu <mabdo@dnaaya.com>
2020-04-27 14:29:22 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./app-tabs/tabs/tabs.module').then(m => m.TabsPageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
   },
   {
     path: 'register',
@@ -22,12 +22,12 @@ const routes: Routes = [
   {
     path: 'maly',
     loadChildren: () => import('./donation-type/maly/maly.module').then( m => m.MalyPageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
   },
   {
     path: 'ayni',
     loadChildren: () => import('./donation-type/ayni/ayni.module').then( m => m.AyniPageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
   },
 
   {
@@ -43,68 +43,68 @@ const routes: Routes = [
   {
     path: 'tabs/tab1/:id2/info/:id',
     loadChildren: () => import('./info/info.module').then( m => m.InfoPageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
   },
   {
     path: 'tabs/tab1/info/:id',
     loadChildren: () => import('./info/info.module').then( m => m.InfoPageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
   },
  
 
   {    path: 'tabs/tab3/:id',
     loadChildren: () => import('./app-tabs/Location/tab3.module').then( m => m.Tab3PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/debt',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/sadka',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/zakah',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/orphans',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/family',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/time',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/utility',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/cloths',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/furniture',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
   {    path: 'tabs/tab1/books',
     loadChildren: () => import('./app-tabs/Charities/tab1.module').then( m => m.Tab1PageModule),
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
 
   },
 ];
